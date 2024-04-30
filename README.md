eba64b322f597059edeacf7dd272dba1c25b49db
strixpanahu <schimpfzach@gmail.com>
2024-04-29 21:58:54 -0500

    Complete steps A, C, D, G, H, & I

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Select any project of your choice. This could range from:
 
-	- Another lab to improve upon.
+	- [x] Another lab to improve upon.
 
 	- A personal project built from scratch.
 
@@ -10,20 +10,22 @@ Select any project of your choice. This could range from:
 
 	- For non-original ideas, we will be checking how you modified the original code by reviewing the features you added (you are expected to make significant changes to the original idea - that's why it is important you attend the project progress meeting to determine how much you need to add/change or if you need to pick a different project altogether because the scope/complexity of the one you've selected is small). Your project should have the items/features below:
 
-	- [ ] A. Code is organized into modules where applicable.
+	- [x] A. Code is organized into modules where applicable.
 
 	- [ ] B. Programs that use/retrieve/store data use files/databases/API.
 
-	- [ ] C. Data validation and exception handling is present where applicable.
+	- [x] C. Data validation and exception handling is present where applicable.
 
-	- [ ] D. Appropriate algorithms/data structures/data types used appropriately.
+	- [x] D. Appropriate algorithms/data structures/data types used appropriately.
 
-	- [ ] E. OOP best practices implemented (Separating code to classes, data hiding, inheritance). Graphical user interface available and intuitive to use where applicable (must separate code in format discussed in lab 10 or test 10). Proper documentation.
+	- [ ] E. OOP best practices implemented (Separating code to classes, data hiding, inheritance). Graphical user interface available and intuitive to use where applicable (must separate code in format discussed in lab 10 or test 10). 
 
-	- [ ] F. Using descriptive variable names, class names, and file names.
+    - [ ] F. Proper documentation.
 
-	- [ ] G. Docstrings for functions.
+	- [x] G. Using descriptive variable names, class names, and file names.
 
-	- [ ] H. Type hinting.
+	- [x] H. Docstrings for functions.
 
-	- [ ] I. Public GitHub link submitted with all project files placed/merged into the main/master branch. The project files could range from python files, images, documents, images, sound files, text files, binary files, executable files, etc. (generally anything that is needed for the application to work).
+	- [x] I. Type hinting.
+
+	- [ ] J. Public GitHub link submitted with all project files placed/merged into the main/master branch. The project files could range from python files, images, documents, images, sound files, text files, binary files, executable files, etc. (generally anything that is needed for the application to work).
--- ZacSchimpf2.py
+++ ZacSchimpf2.py
@@ -7,143 +7,138 @@
 """
 
 
-def main():
-    """
-    Main logic structure that accepts students & scores, creates grade scale, and prints results
-    :return: None
-    """
+class Grader:
+    def __init__(self) -> None:
+        """
+        Main logic structure that accepts students & scores, creates grade scale, and prints results
+        :return: None
+        """
 
-    students = get_students()
-    scores = get_scores(students)
-    grade_scale = get_grade_scale(scores)
-    output(scores, grade_scale)
+        students = self.get_students()
+        scores = self.get_scores(students)
+        grade_scale = self.get_grade_scale(scores)
+        self.__str__(scores, grade_scale)
 
+    def get_students(self) -> int:
+        """
+        Requests input of quantity of students
+        :return: int value of students
+        """
 
-def get_students():
-    """
-    Requests input of quantity of students
-    :return: int value of students
-    """
+        students = input("Total number of students: ").strip()
+        try:
+            students = int(students)
 
-    students = input("Total number of students: ").strip()
-    try:
-        students = int(students)
+            if students < 0:
+                raise IndexError
 
-        if students < 0:
-            raise IndexError
+        except IndexError:
+            print("\"" + str(students) + "\" must be a positive whole-number integer value")
+            students = self.get_students()
 
-    except IndexError:
-        print("\"" + str(students) + "\" must be a positive whole-number integer value")
-        students = get_students()
+        except ValueError:
+            print("\"" + str(students) + "\" must be a whole-number integer value, please try again.")
+            students = self.get_students()
 
-    except ValueError:
-        print("\"" + str(students) + "\" must be a whole-number integer value, please try again.")
-        students = get_students()
+        return students
 
-    return students
+    def get_scores(self, students) -> list:
+        """
+        Requests input of score and appends letter grade scale
+        :param students: int quantity of students
+        :return: List[] containing int scores; e.g. [100, 0]
+        """
 
+        try:
+            scores = self.scores_input(students)
+            scores = self.convert_to_list(students, scores)
 
-def get_scores(students):
-    """
-    Requests input of score and appends letter grade scale
-    :param students: int quantity of students
-    :return: List[] containing int scores; e.g. [100, 0]
-    """
+        except ValueError:
+            print("Scores must only include whole-number integer values between 0-100.")
+            scores = self.get_scores(students)
 
-    try:
-        scores = scores_input(students)
-        scores = convert_to_list(students, scores)
+        except IndexError:
+            print("Scores contains the incorrect amount space-separated values for each student. "
+                  + "Please provide a minimum of " + str(students) + " values.")
+            scores = self.get_scores(students)
 
-    except ValueError:
-        print("Scores must only include whole-number integer values between 0-100.")
-        scores = get_scores(students)
-
-    except IndexError:
-        print("Scores contains the incorrect amount space-separated values for each student. "
-              + "Please provide a minimum of " + str(students) + " values.")
-        scores = get_scores(students)
-
-    return scores
-
-
-def scores_input(students):
-    """
-    Accepts user input, cleans, and validates spacing
-    :param students: int value of students
-    :return: String value user input; e.g. "100 70 50"
-    """
-
-    scores = input("Enter " + str(students) + " score(s): ").strip()
-
-    spaces_count = scores.count(' ')
-    score_breaks = students - 1
-    if spaces_count < score_breaks:
-        raise IndexError
-    else:
         return scores
 
+    def scores_input(self, students) -> str:
+        """
+        Accepts user input, cleans, and validates spacing
+        :param students: int value of students
+        :return: String value user input; e.g. "100 70 50"
+        """
 
-def convert_to_list(students, scores_str):
-    """
-    Processes conversion of values and creates grade scale
-    :param scores_str: Space-separated integers within a String; e.g. "100 70 50"
-    :param students: int quantity of students
-    :return:  List[] containing int scores; e.g. [100, 0]
-    """
+        scores = input("Enter " + str(students) + " score(s): ").strip()
 
-    scores_list = scores_str.split()
-    scores_list = list(map(int, scores_list))
-    scores_list = scores_list[slice(students)]
+        spaces_count = scores.count(' ')
+        score_breaks = students - 1
+        if spaces_count < score_breaks:
+            raise IndexError
+        else:
+            return scores
 
-    for current_score in scores_list:
-        if current_score not in range(0, 101):
-            raise ValueError
+    def convert_to_list(self, students, scores_str) -> list:
+        """
+        Processes conversion of values and creates grade scale
+        :param scores_str: Space-separated integers within a String; e.g. "100 70 50"
+        :param students: int quantity of students
+        :return:  List[] containing int scores; e.g. [100, 0]
+        """
 
-    return scores_list
+        scores_list = scores_str.split()
+        scores_list = list(map(int, scores_list))
+        scores_list = scores_list[slice(students)]
+
+        for current_score in scores_list:
+            if current_score not in range(0, 101):
+                raise ValueError
+
+        return scores_list
+
+    def get_grade_scale(self, scores) -> None:
+        """
+        Create weighted grade scale & append to provided scores
+        :param scores: List[] containing int scores; e.g. [100, 0]
+        :return: Dict{} containing A-F minimum scores; e.g. {A:90, B:80, C:70, D:60, F:59}
+        """
+
+        best_score = max(scores)
+
+        grading_scale = {'A': best_score - 10,
+                         'B': best_score - 20,
+                         'C': best_score - 30,
+                         'D': best_score - 40,
+                         'F': best_score - 41}
+
+        return grading_scale
+
+    def __str__(self, scores, grade_scale) -> str:
+        """
+        Console-level output of students' scores & letter grades
+        :param scores: List[] containing int scores; e.g. [100, 0]
+        :param grade_scale: Dict{} containing A-F minimum scores; e.g. {A:90, B:80, C:70, D:60, F:59}
+        :return: None
+        """
+
+        iteration = 1
+        for current_score in scores:
+            match current_score:
+                case value if value in range(grade_scale.get('A'), (max(scores) + 1)):
+                    current_grade = 'A'
+                case value if value in range(grade_scale.get('B'), grade_scale.get('A')):
+                    current_grade = 'B'
+                case value if value in range(grade_scale.get('C'), grade_scale.get('B')):
+                    current_grade = 'C'
+                case value if value in range(grade_scale.get('D'), grade_scale.get('C')):
+                    current_grade = 'D'
+                case _:
+                    current_grade = 'F'
+
+            print("Student " + str(iteration) + " score is " + str(current_score) + " and grade is " + current_grade)
+            iteration += 1
 
 
-def get_grade_scale(scores):
-    """
-    Create weighted grade scale & append to provided scores
-    :param scores: List[] containing int scores; e.g. [100, 0]
-    :return: Dict{} containing A-F minimum scores; e.g. {A:90, B:80, C:70, D:60, F:59}
-    """
-
-    best_score = max(scores)
-
-    grading_scale = {'A': best_score - 10,
-                     'B': best_score - 20,
-                     'C': best_score - 30,
-                     'D': best_score - 40,
-                     'F': best_score - 41}
-
-    return grading_scale
-
-
-def output(scores, grade_scale):
-    """
-    Console-level output of students' scores & letter grades
-    :param scores: List[] containing int scores; e.g. [100, 0]
-    :param grade_scale: Dict{} containing A-F minimum scores; e.g. {A:90, B:80, C:70, D:60, F:59}
-    :return: None
-    """
-
-    iteration = 1
-    for current_score in scores:
-        match current_score:
-            case value if value in range(grade_scale.get('A'), (max(scores) + 1)):
-                current_grade = 'A'
-            case value if value in range(grade_scale.get('B'), grade_scale.get('A')):
-                current_grade = 'B'
-            case value if value in range(grade_scale.get('C'), grade_scale.get('B')):
-                current_grade = 'C'
-            case value if value in range(grade_scale.get('D'), grade_scale.get('C')):
-                current_grade = 'D'
-            case _:
-                current_grade = 'F'
-
-        print("Student " + str(iteration) + " score is " + str(current_score) + " and grade is " + current_grade)
-        iteration += 1
-
-
-main()
+test = Grader()
